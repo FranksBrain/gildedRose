c5f84c7016ae7928324ae98557494afdc4526862
Frank DeLuca <frank@franksbrain.com>
2020-04-09 20:48:37 -0400

    - Create method for returning item list

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -5,13 +5,27 @@ namespace csharpcore
 {
     public class GildedRoseUpdateQuality
     {
+        private IList<Item> GetNormalItem()
+        {
+            return new List<Item> { new Item { Name = "foo", SellIn = 10, Quality = 9 } };
+        }
+        
         [Fact]
         public void ReducesNormalItemSellInByOne()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 10, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
+            IList<Item> items = GetNormalItem();
+            GildedRose app = new GildedRose(items);
             app.UpdateQuality();
-            Assert.Equal(9, Items[0].SellIn);
+            Assert.Equal(9, items[0].SellIn);
+        }
+
+        [Fact]
+        public void ReducesNormalItemQualityByOne()
+        {
+            IList<Item> items = GetNormalItem();
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.Equal(8, items[0].Quality);
         }
     }
 }

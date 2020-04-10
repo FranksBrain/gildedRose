a7d5b654a5e503f4c3155083489c7f710c77395f
Frank DeLuca <frank@franksbrain.com>
2020-04-09 20:43:56 -0400

    - Pass approval test - Create first test

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 
 namespace csharpcore
 {
-    public class GildedRoseTest
+    public class GildedRoseUpdateQuality
     {
         [Fact]
-        public void foo()
+        public void ReducesNormalItemSellInByOne()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 10, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+            Assert.Equal(9, Items[0].SellIn);
         }
     }
 }

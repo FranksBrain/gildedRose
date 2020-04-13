c2226470d0b8365816a4c13d309a1cb23b5ef827
Frank DeLuca <frank@franksbrain.com>
2020-04-13 18:09:10 -0400

    - Refactor GetNormalItem - Add constant default values

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -5,7 +5,10 @@ namespace csharpcore
 {
     public class GildedRoseUpdateQuality
     {
-        private Item GetNormalItem(int sellIn, int quality)
+        private const int _defaultSellIn = 10;
+        private const int _defaultQuality = 20;
+
+        private Item GetNormalItem(int sellIn = _defaultSellIn, int quality = _defaultQuality)
         {
             return new Item { Name = "foo", SellIn = sellIn, Quality = quality };
         }
@@ -13,28 +16,28 @@ namespace csharpcore
         [Fact]
         public void ReducesNormalItemSellInByOne()
         { 
-            IList<Item> items = new List<Item> { GetNormalItem(10, 9) };
+            IList<Item> items = new List<Item> { GetNormalItem() };
             GildedRose app = new GildedRose(items);
             app.UpdateQuality();
-            Assert.Equal(9, items[0].SellIn);
+            Assert.Equal(_defaultSellIn - 1, items[0].SellIn);
         }
 
         [Fact]
         public void ReducesNormalItemQualityByOne()
         {
-            IList<Item> items = new List<Item> { GetNormalItem(10, 9) };
+            IList<Item> items = new List<Item> { GetNormalItem() };
             GildedRose app = new GildedRose(items);
             app.UpdateQuality();
-            Assert.Equal(8, items[0].Quality);
+            Assert.Equal(_defaultQuality - 1, items[0].Quality);
         }
 
         [Fact]
         public void QualityDegradesTwiceAsFastOnceSellInPasses()
         {
-            IList<Item> items = new List<Item> { GetNormalItem(0, 10) };
+            IList<Item> items = new List<Item> { GetNormalItem(sellIn: 0) };
             GildedRose app = new GildedRose(items);
             app.UpdateQuality();
-            Assert.Equal(8, items[0].Quality);
+            Assert.Equal(_defaultQuality - 2, items[0].Quality);
         }
     }
 }

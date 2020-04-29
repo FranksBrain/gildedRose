44bd101ac21b59d097c718ac82bd0b3da20a2e04
Frank DeLuca <frank@franksbrain.com>
2020-04-29 18:16:08 -0400

    - clean up backstage rule

--- Rules/BackstagePassesRule.cs
+++ Rules/BackstagePassesRule.cs
@@ -6,29 +6,24 @@
         {
             if(item.Name == ItemName.BACKSTAGE_PASSES)
             {
+                item.Quality += 1;
+
+                if (item.SellIn < 11)
+                {
+                    item.Quality += 1;
+                }
+
+                if (item.SellIn < 6)
+                {
+                    item.Quality += 1;
+                }
+
                 if (item.SellIn <= 0)
                 {
                     item.Quality = 0;
-                    item.SellIn -= 1;
                 }
 
-                if (item.SellIn <= 5 && item.SellIn > 0)
-                {
-                    item.Quality += 3;
-                    item.SellIn -= 1;
-                }
-
-                if (item.SellIn <= 10 && item.SellIn > 5)
-                {
-                    item.Quality += 2;
-                    item.SellIn -= 1;
-                }
-              
-                if (item.SellIn > 10)
-                {
-                    item.Quality += 1;
-                    item.SellIn -= 1;
-                }
+                item.SellIn -= 1;
             }
         }
     }
